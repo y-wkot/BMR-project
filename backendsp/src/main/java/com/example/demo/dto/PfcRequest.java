3efddc83501d7f1bd7a0c3220f144f51415f0830
yk <y.spada1017@gmail.com>
2025-05-07 20:58:26 +0900

    250507ｃ

--- backendsp/src/main/java/com/example/demo/dto/PfcRequest.java
+++ backendsp/src/main/java/com/example/demo/dto/PfcRequest.java
@@ -5,7 +5,10 @@ public class PfcRequest {
 	private int age; // 年齢
 	private double height; // 身長
 	private double weight; // 体重
-	private double bmr;
+	private double bmr; // 基礎代謝
+	private double proteinFactor; // たんぱくしつ量
+	private double fatGram; // 脂質量
+	private double carbGram; // 炭水化物量
 
 	public double getBmr() {
 		return bmr;
@@ -45,6 +48,30 @@ public class PfcRequest {
 
 	public void setGender(String gender) {
 		this.gender = gender;
+	}
+
+	public double getProteinFactor() {
+		return proteinFactor;
+	}
+
+	public void setProteinFactor(double proteinFactor) {
+		this.proteinFactor = proteinFactor;
+	}
+
+	public double getFatGram() {
+		return fatGram;
+	}
+
+	public void setFatGram(double fatGram) {
+		this.fatGram = fatGram;
+	}
+
+	public double getCarbGram() {
+		return carbGram;
+	}
+
+	public void setCarbGram(double carbGram) {
+		this.carbGram = carbGram;
 	}
 
 }
