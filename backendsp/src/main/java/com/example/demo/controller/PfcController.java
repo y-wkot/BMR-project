233ede0ae6416605acbad262880c2711151aba9c
yk <y.spada1017@gmail.com>
2025-05-08 00:04:09 +0900

    250508aデータベーススキップ

--- backendsp/src/main/java/com/example/demo/controller/PfcController.java
+++ backendsp/src/main/java/com/example/demo/controller/PfcController.java
@@ -1,9 +1,8 @@
 package com.example.demo.controller;
 
-import jakarta.servlet.http.HttpSession; // ← 追加
+import jakarta.servlet.http.HttpSession;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus; // ← 修正ポイント
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -11,14 +10,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.dto.PfcRequest;
-import com.example.demo.service.PfcService;
 
 @RestController
 @RequestMapping("/api/pfc")
 public class PfcController {
 
-  @Autowired
-  private PfcService pfcService;
+//  @Autowired
+//  private PfcService pfcService; データベース未実装のためスキップさせる
 
   @PostMapping("/save")
   public ResponseEntity<?> savePfc(@RequestBody PfcRequest request, HttpSession session) {
@@ -27,7 +25,7 @@ public class PfcController {
       return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("ログインしていません");
     }
 
-    pfcService.save(userId, request);
+//    pfcService.save(userId, request);　同じく
     return ResponseEntity.ok("保存完了");
   }
 }
--- backendsp/src/main/java/com/example/demo/service/PfcService.java
+++ backendsp/src/main/java/com/example/demo/service/PfcService.java
@@ -1,7 +1,5 @@
 package com.example.demo.service;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Service;
 
 import com.example.demo.dto.PfcRequest;
@@ -9,12 +7,12 @@ import com.example.demo.dto.PfcRequest;
 @Service
 public class PfcService {
 
-  @Autowired
-  private JdbcTemplate jdbc;
+//  @Autowired
+//  private JdbcTemplate jdbc;　データベース未実装のためスキップ
 
   public void save(int userId, PfcRequest request) {
-    String sql = "UPDATE member_info SET weight=?, bmr=?, protein_factor=?, fat=?, carb=? WHERE id=?";
-    jdbc.update(sql,request.getWeight(),request.getBmr(),request.getProteinFactor(),request.getFatGram(),request.getCarbGram(),      // 同上
-    		  userId);
+//    String sql = "UPDATE member_info SET weight=?, bmr=?, protein_factor=?, fat=?, carb=? WHERE id=?";
+//    jdbc.update(sql,request.getWeight(),request.getBmr(),request.getProteinFactor(),request.getFatGram(),request.getCarbGram(),      // 同上
+//    		  userId);
   }
 }
